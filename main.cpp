25347a510b4d61781e45244f24efce51514861d6
VictorNorman <vtn2@calvin.edu>
2023-07-10 14:35:19 -0400

    provide code to parse arg

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+#define MAX_SQU_SIZE 15
+
+// Run the executable this way:
+// ./proj2 5                      // to make a magic square of size 5
+// ./proj2 15                     // max size of 15.
+int main(int argc, char *argv[]) {
+    int size;  // size of magic square
+
+    size = atoi(argv[1]);
 
     return 0;
 }
